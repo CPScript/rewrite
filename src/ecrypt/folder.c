3831df06454e06bac196f3a3393fa69e257de936
Disease <83523587+CPScript@users.noreply.github.com>
2024-12-21 04:35:57 -0500

    Create folder.c

---
+++ src/ecrypt/folder.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include <direct.h>
+#define mkdir(name) _mkdir(name)
+#else
+#include <sys/stat.h>
+#include <sys/types.h>
+#endif
+
+int main() {
+    #ifdef _WIN32
+        char* desktopPath = getenv("USERPROFILE");
+        if (desktopPath == NULL) {
+            fprintf(stderr, "Failed to get the user's profile path.\n");
+            return 1;
+        }
+        char folderPath[1024];
+        snprintf(folderPath, sizeof(folderPath), "%s\\Desktop\\Photos", desktopPath);
+    #endif
+
+    if (mkdir(folderPath) == 0) {
+        printf("Folder made\n");
+    } else {
+        perror("Failed to make folder");
+        return 1;
+    }
+
+    return 0;
+}
